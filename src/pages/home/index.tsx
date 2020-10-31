1a5161eb309f6407ae895102f4a458e02a877560
Michael Zhou <mz25@illinois.edu>
2020-10-31 05:43:11 -0500

    finish draft 1 of home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,6 +9,9 @@ import RIGHT_TREES from '../../assets/home/right_trees.svg';
 import WIND from '../../assets/home/wind.svg';
 import CAMPERS from '../../assets/home/campers.svg';
 import LOGO from '../../assets/home/logo.svg';
+import MOON_SHADOW from '../../assets/home/moon_shadow.svg';
+import MOBILE_TREES from '../../assets/home/mobile_trees.svg';
+import LOGO_MOBILE from '../../assets/home/logo_mobile.svg';
 
 const Home: React.FC = () => (
   <div className="home">
@@ -22,7 +25,10 @@ const Home: React.FC = () => (
     <div className="home-wind" style={{ backgroundImage: `url(${WIND})` }} />
     <div className="home-campers" style={{ backgroundImage: `url(${CAMPERS})` }} />
     <div className="home-logo" style={{ backgroundImage: `url(${LOGO})` }} />
-    <div className="home-landing-text">Rekindling Connections</div>
+    <div className="home-landing-text">REKINDLING CONNECTIONS</div>
+    <div className="home-moon-shadow" style={{ backgroundImage: `url(${MOON_SHADOW})` }} />
+    <div className="home-mobile" style={{ backgroundImage: `url(${MOBILE_TREES})` }} />
+    <div className="home-logo-mobile" style={{ backgroundImage: `url(${LOGO_MOBILE})` }} />
   </div>
 );
 
