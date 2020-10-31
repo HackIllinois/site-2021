7d37bf021f250d146d9f01685c280176d479d07a
Michael Zhou <mz25@illinois.edu>
2020-10-31 02:39:48 -0500

    finish adding all elements onto front page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,6 +6,9 @@ import BOTTOM_LANDSCAPE from '../../assets/home/bottom_landscape.svg';
 import MOUNTAINS from '../../assets/home/mountains.svg';
 import LEFT_TREES from '../../assets/home/left_trees.svg';
 import RIGHT_TREES from '../../assets/home/right_trees.svg';
+import WIND from '../../assets/home/wind.svg';
+import CAMPERS from '../../assets/home/campers.svg';
+import LOGO from '../../assets/home/logo.svg';
 
 const Home: React.FC = () => (
   <div className="home">
@@ -16,6 +19,10 @@ const Home: React.FC = () => (
     <div className="home-water" />
     <div className="home-left-trees" style={{ backgroundImage: `url(${LEFT_TREES})` }} />
     <div className="home-right-trees" style={{ backgroundImage: `url(${RIGHT_TREES})` }} />
+    <div className="home-wind" style={{ backgroundImage: `url(${WIND})` }} />
+    <div className="home-campers" style={{ backgroundImage: `url(${CAMPERS})` }} />
+    <div className="home-logo" style={{ backgroundImage: `url(${LOGO})` }} />
+    <div className="home-landing-text">Rekindling Connections</div>
   </div>
 );
 
