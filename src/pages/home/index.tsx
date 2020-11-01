3a9e5d3a5dda112c78933651c805769fddd940cf
Michael Zhou <mz25@illinois.edu>
2020-10-31 20:08:45 -0500

    update homepage to css modules

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles.scss';
+import styles from './styles.module.scss';
 
 import STARS from '../../assets/home/stars.svg';
 import BOTTOM_LANDSCAPE from '../../assets/home/bottom_landscape.svg';
@@ -14,21 +14,21 @@ import MOBILE_TREES from '../../assets/home/mobile_trees.svg';
 import LOGO_MOBILE from '../../assets/home/logo_mobile.svg';
 
 const Home: React.FC = () => (
-  <div className="home">
-    <div className="home-stars" style={{ backgroundImage: `url(${STARS})` }} />
-    <div className="home-moon" />
-    <div className="home-bottom-landscape" style={{ backgroundImage: `url(${BOTTOM_LANDSCAPE})` }} />
-    <div className="home-mountains" style={{ backgroundImage: `url(${MOUNTAINS})` }} />
-    <div className="home-water" />
-    <div className="home-left-trees" style={{ backgroundImage: `url(${LEFT_TREES})` }} />
-    <div className="home-right-trees" style={{ backgroundImage: `url(${RIGHT_TREES})` }} />
-    <div className="home-wind" style={{ backgroundImage: `url(${WIND})` }} />
-    <div className="home-campers" style={{ backgroundImage: `url(${CAMPERS})` }} />
-    <div className="home-logo" style={{ backgroundImage: `url(${LOGO})` }} />
-    <div className="home-landing-text">REKINDLING CONNECTIONS</div>
-    <div className="home-moon-shadow" style={{ backgroundImage: `url(${MOON_SHADOW})` }} />
-    <div className="home-mobile" style={{ backgroundImage: `url(${MOBILE_TREES})` }} />
-    <div className="home-logo-mobile" style={{ backgroundImage: `url(${LOGO_MOBILE})` }} />
+  <div className={styles.home}>
+    <div className={styles.stars} style={{ backgroundImage: `url(${STARS})` }} />
+    <div className={styles.moon} />
+    <div className={styles.bottomLandscape} style={{ backgroundImage: `url(${BOTTOM_LANDSCAPE})` }} />
+    <div className={styles.mountains} style={{ backgroundImage: `url(${MOUNTAINS})` }} />
+    <div className={styles.water} />
+    <div className={styles.leftTrees} style={{ backgroundImage: `url(${LEFT_TREES})` }} />
+    <div className={styles.rightTrees} style={{ backgroundImage: `url(${RIGHT_TREES})` }} />
+    <div className={styles.wind} style={{ backgroundImage: `url(${WIND})` }} />
+    <div className={styles.campers} style={{ backgroundImage: `url(${CAMPERS})` }} />
+    <div className={styles.logo} style={{ backgroundImage: `url(${LOGO})` }} />
+    <div className={styles.landingText}>REKINDLING CONNECTIONS</div>
+    <div className={styles.moonShadow} style={{ backgroundImage: `url(${MOON_SHADOW})` }} />
+    <div className={styles.mobile} style={{ backgroundImage: `url(${MOBILE_TREES})` }} />
+    <div className={styles.logoMobile} style={{ backgroundImage: `url(${LOGO_MOBILE})` }} />
   </div>
 );
 
