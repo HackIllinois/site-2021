d23010556fc7351e6caca2883ba3ba1499c791f1
Romir Singla <romirsingla@Romirs-Macbook.local>
2020-11-04 20:36:24 -0600

    removing mixins in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 
 function App(): JSX.Element {
   return (
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Section1 from './Section1';
-import Section2 from './Section2';
+import Hero from './Hero/index';
+// import EventInfo from './EventInfo';
 
 const Home: React.FC = () => (
   <>
-    <Section1 />
-    <Section2 />
+    <Hero />
+    {/* <EventInfo /> */}
   </>
 );
 
