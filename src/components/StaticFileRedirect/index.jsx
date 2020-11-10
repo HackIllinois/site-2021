b38f58262bc58f93d8302cfb06e3e35e5e374e6d
Sarthak Kamboj <sarthakkamboj22@gmail.com>
2020-11-09 16:01:26 -0800

    commented google analytics part

--- src/components/StaticFileRedirect/index.jsx
+++ src/components/StaticFileRedirect/index.jsx
@@ -4,10 +4,10 @@ const StaticFileRedirect = ({ to }) => {
   const redirect = () => window.location.replace(to);
 
   // Record the pageview to google analytics before redirect
-  ReactGA.set({ page: to });
-  ReactGA.ga('send', 'pageview', to, {
-    hitCallback: redirect,
-  });
+ //  ReactGA.set({ page: to });
+  // ReactGA.ga('send', 'pageview', to, {
+    // hitCallback: redirect,
+  // });
 
   // In case google analytics is down or request fails for some reason, continue to redirect after timeout
   setTimeout(redirect, 1000);
