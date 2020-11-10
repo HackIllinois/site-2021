700c45eac331ed3daf40d5de45713e75f214fc70
Sarthak Kamboj <sarthakkamboj22@gmail.com>
2020-11-09 17:36:40 -0800

    added types to StaticFileRedirect

---
+++ src/components/StaticFileRedirect/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+// import ReactGA from "react-ga";
+
+type propTypes = {
+    to: string;
+};
+
+const StaticFileRedirect: React.FC<propTypes> = ({ to }) => {
+    const redirect = () => window.location.replace(to);
+
+    // Record the pageview to google analytics before redirect
+    //  ReactGA.set({ page: to });
+    // ReactGA.ga('send', 'pageview', to, {
+    // hitCallback: redirect,
+    // });
+
+    // In case google analytics is down or request fails for some reason, continue to redirect after timeout
+    setTimeout(redirect, 1000);
+
+    return null;
+};
+
+export default StaticFileRedirect;
