5a5fd3030868fcd2b995ab6d74be762a743a4d4e
Romir Singla <romirsingla@Romirs-Macbook.local>
2020-12-02 21:26:53 -0600

    setup registration file structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from 'pages/Home';
 import Auth from 'pages/Auth';
+import Registration from 'pages/Registration';
 
 function App(): JSX.Element {
   return (
@@ -14,6 +15,10 @@ function App(): JSX.Element {
 
         <Route path="/auth" exact>
           <Auth />
+        </Route>
+
+        <Route path="/registration" exact>
+          <Registration />
         </Route>
       </Switch>
     </Router>
---
+++ src/pages/Registration/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Background from './Background';
+import Navigation from './Navigation';
+
+const Registration: React.FC = () => (
+  <>
+    <Background/>
+    <Navigation/>
+  </>
+);
+
+export default Registration;
