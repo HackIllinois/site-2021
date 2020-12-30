f93c68d4832b16d85aa185dc0416d236d991a461
SarthakKamboj <sarthakkamboj22@gmail.com>
2020-12-29 18:06:45 -0800

    added ga events and pageviews

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -2,40 +2,46 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import queryString from 'query-string';
 import { useLocation } from 'react-router-dom';
-
+import ReactGa from 'react-ga';
 import { getToken } from 'api';
 // import Loading from 'components/Loading';
 
-function mobileRedirect(os, code) {
-  const to = `hackillinois://org.hackillinois.${os}/auth?code=${code}`;
-  window.location.replace(to);
-}
 
-const Auth = () => {
-  const location = useLocation();
-  const {
-    code,
-    isAndroid,
-    isiOS,
-    to,
-  } = queryString.parse(location.search);
 
-  if (!code) {
-    window.location.replace('/');
-  }
+const Auth: React.FC = () => {
 
-  if (isAndroid || isiOS) {
-    const os = isAndroid ? 'android' : 'ios';
-    mobileRedirect(os, code);
-  } else {
-    getToken(code).then((token) => {
-      sessionStorage.setItem('token', token);
+  useEffect(() => {
+    function mobileRedirect(os: string, code) {
+      const to = `hackillinois://org.hackillinois.${os}/auth?code=${code}`;
+      ReactGa.event(os);
       window.location.replace(to);
-    });
-  }
+    }
+    const location = useLocation();
+    const {
+      code,
+      isAndroid,
+      isiOS,
+      to,
+    } = queryString.parse(location.search);
+
+    if (!code) {
+      window.location.replace('/');
+    }
+
+    if (isAndroid || isiOS) {
+      const os = isAndroid ? 'android' : 'ios';
+      mobileRedirect(os, code);
+    } else {
+      getToken(code).then((token) => {
+        sessionStorage.setItem('token', token);
+        window.location.replace(to);
+      });
+    }
+    ReactGa.pageview("/Auth");
+  },[]);
 
   return <div>Loading...</div>; // <Loading />;
 }
