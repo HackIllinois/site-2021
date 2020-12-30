22f7509d670f5cfba9b2bb332ff0894e860091d0
SarthakKamboj <sarthakkamboj22@gmail.com>
2020-12-29 20:59:42 -0800

    added events for mobile

--- src/components/AuthenticatedRoute/index.tsx
+++ src/components/AuthenticatedRoute/index.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-<<<<<<< HEAD
-import { isAuthenticated, authenticate } from 'api';
-=======
 import { isAuthenticated, authenticate } from 'util/api';
->>>>>>> 38ff90a4ea5a1673c5047817a3c3678b4a8573cf
 
 type AuthenticatedRoutePropTypes = {
   path: string;
 };
 
-<<<<<<< HEAD
-const AuthenticatedRoute: React.FC<AuthenticatedRoutePropTypes> = (props) => {
-=======
 const AuthenticatedRoute = (props: AuthenticatedRoutePropTypes): JSX.Element => {
->>>>>>> 38ff90a4ea5a1673c5047817a3c3678b4a8573cf
   const { path } = props;
   if (!isAuthenticated()) {
     authenticate(path);
