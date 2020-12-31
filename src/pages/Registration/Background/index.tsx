dfe5bca9d1d229f96db2053daa1a053d10b12cec
Romir Singla <romirsingla@Romirs-Macbook.local>
2020-12-30 21:55:13 -0600

    started Registration form

--- src/pages/Registration/Background/index.tsx
+++ src/pages/Registration/Background/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+// import STRING from 'assets/registration/navigation/string.svg';
+
 import styles from './styles.module.scss';
 
 const Background: React.FC = () => (
+  <div className={styles.background} />
 );
 
 export default Background;
--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Background from './Background';
 import Navigation from './Navigation';
+import Form from './Form';
 
-const Registration: React.FC = () => (
-  <>
-    <Background/>
-    <Navigation/>
-  </>
-);
+const Registration: React.FC = () => {
+  const [formIndex, setFormIndex] = useState('0');
+  //   const [prevTab, setPrevTab] = useState<HTMLElement>();
+
+  const toggleIndex = (e: { target: HTMLDivElement }) => {
+    setFormIndex(e.target.getAttribute('id') as string);
+    // e.target.style.background = '#FED684';
+    // if (prevTab) {
+    //   prevTab.style.background = '#85692F';
+    //   setPrevTab(e.target);
+    // }
+  };
+
+  return (
+    <>
+      <Background />
+      <Navigation handleClick={toggleIndex} />
+      <Form formIndex={formIndex} />
+    </>
+  );
+};
 
 export default Registration;
