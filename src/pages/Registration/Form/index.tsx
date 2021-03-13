68d2130d9267571084b169eb67b78fd0a2316629
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-03-13 11:07:56 -0600

    Connect to API

--- src/pages/Registration/Form/index.tsx
+++ src/pages/Registration/Form/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { useForm, SubmitHandler, SubmitErrorHandler, FormProvider } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
+import { RegistrationType } from 'util/types';
+import { getRegistration, getRoles, refreshToken, register } from 'util/api';
 import PROJECTOR from 'assets/registration/projector.svg';
 import Button from 'components/form/Button';
 import { registrationSchema, RegistrationSchema, errorMap, defaultValues } from '../validation';
@@ -11,9 +13,9 @@ import PersonalInfo from './screens/PersonalInfo';
 import RaceDemographics from './screens/RaceDemographics';
 import Education from './screens/Education';
 import Experience from './screens/Experience';
+import Finish from './screens/Finish';
 
 import styles from './styles.module.scss';
-import Finish from './screens/Finish';
 
 type FormProps = {
   formIndex: number,
@@ -32,16 +34,59 @@ const fields: (keyof RegistrationSchema)[][] = [
 const pages = [Welcome, PersonalInfo, RaceDemographics, Education, Experience, Finish];
 const submitPageIndex = pages.length - 2;
 
+const convertToAPI = (data: RegistrationSchema): RegistrationType => {
+  const { name, race: possibleRace, ...registration } = data;
+  const [firstName, ...remainingWords] = name.split(' ');
+  const lastName = remainingWords.join(' ') || ' ';
+  const race = possibleRace.length === 0 ? ['Prefer Not to Answer'] : possibleRace;
+  return { ...registration, firstName, lastName, race };
+};
+
+const convertFromAPI = (registration: RegistrationType): RegistrationSchema => {
+  const { firstName, lastName, ...remaining } = registration;
+  const name = firstName ? (`${firstName} ${lastName}`).trim() : '';
+  return { ...remaining, name };
+};
+
 const Form = ({ formIndex, setFormIndex }: FormProps): JSX.Element => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+
   const methods = useForm<RegistrationSchema>({
     resolver: zodResolver(registrationSchema, { errorMap }),
     defaultValues,
   });
   const { handleSubmit } = methods;
 
+  useEffect(() => {
+    getRoles().then((roles) => {
+      if (roles.includes('Applicant')) {
+        setIsEditing(true);
+        return getRegistration('attendee');
+      }
+      return null;
+    }).then((registrationWithId) => {
+      if (registrationWithId) {
+        const { id, ...registration } = registrationWithId;
+        methods.reset(convertFromAPI(registration));
+      }
+    }).finally(() => {
+      setIsLoading(false);
+    });
+  }, []); // deliberately not including `methods`
+
   const onSubmit: SubmitHandler<RegistrationSchema> = (data) => {
     console.log(data);
-    setFormIndex(pages.length - 1);
+
+    setIsLoading(true);
+    return register(isEditing, 'attendee', convertToAPI(data)).then(() => {
+      setFormIndex(pages.length - 1);
+      refreshToken(); // token changes after registration, so need to refetch
+    }).catch(() => {
+      alert('There was an error while submitting. If this error persists, please email contact@hackillinois.org');
+    }).finally(() => {
+      setIsLoading(false);
+    });
   };
 
   const onError: SubmitErrorHandler<RegistrationSchema> = (errorData) => {
@@ -72,8 +117,9 @@ const Form = ({ formIndex, setFormIndex }: FormProps): JSX.Element => {
                 <div className={styles.buttons}>
                   <Button arrow="left" hidden={formIndex === 0} onClick={previousPage} />
                   <div className={styles.spacer} />
-                  {(formIndex !== submitPageIndex) && <Button arrow="right" onClick={nextPage}>Next</Button>}
-                  {(formIndex === submitPageIndex) && <Button type="submit">Submit</Button>}
+                  {isLoading && <Button loading>Loading...</Button>}
+                  {(!isLoading && formIndex !== submitPageIndex) && <Button arrow="right" onClick={nextPage}>Next</Button>}
+                  {(!isLoading && formIndex === submitPageIndex) && <Button type="submit">Submit</Button>}
                 </div>
               )}
             </div>
