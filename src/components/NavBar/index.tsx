f45ba36e2a0756293a211be9fb8b2ef0fb730a8a
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-04-06 22:20:35 -0500

    Complete nav bar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
+import { Link } from 'react-router-dom';
 
 import LOGO from 'assets/logo.svg';
 import styles from './styles.module.scss';
 import AnimatedLink from './AnimatedLink';
+import HighlightedLink from './HighlightedLink';
+import MenuIcon from './MenuIcon';
 
 type LinkType = { text: string, to: string };
 const links: LinkType[] = [
@@ -18,19 +21,75 @@ const linksWithHome = [{ text: 'Home', to: '/' }].concat(links);
 type PropTypes = {
   hideLogo?: boolean;
   showHome?: boolean;
+  mobileBreakpoint?: number;
   className?: string;
 };
 
-const NavBar = ({ hideLogo, showHome, className }: PropTypes): JSX.Element => (
-  <div className={clsx(styles.navBar, className)}>
-    <img className={clsx(styles.logo, hideLogo && styles.hidden)} src={LOGO} alt="HackIllinois Logo" />
+const NavBar = ({ hideLogo, showHome, mobileBreakpoint = 768, className }: PropTypes): JSX.Element => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < mobileBreakpoint);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
 
-    <div className={styles.spacer} />
+  useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${mobileBreakpoint}px)`);
+    const listener = (e: MediaQueryListEvent) => {
+      setIsMobile(e.matches);
+      if (!e.matches) { // not mobile
+        setIsSideBarOpen(false);
+      }
+    };
+    mql.addEventListener('change', listener);
+    return () => mql.removeEventListener('change', listener);
+  }, [mobileBreakpoint]);
 
-    {(showHome ? linksWithHome : links).map(({ text, to }) => (
-      <AnimatedLink className={styles.link} to={to}>{text}</AnimatedLink>
-    ))}
-  </div>
-);
+  const linksToUse = showHome ? linksWithHome : links;
+
+  return (
+    <header>
+      <nav className={clsx(styles.navBar, className)}>
+        <Link to="/">
+          <img className={clsx(styles.logo, hideLogo && styles.hidden)} src={LOGO} alt="HackIllinois Logo" />
+        </Link>
+
+        <div className={styles.spacer} />
+
+        {isMobile ? (
+          <button
+            type="button"
+            className={styles.sideBarToggle}
+            onClick={() => setIsSideBarOpen(true)}
+            aria-label="Open Side Bar"
+          >
+            <MenuIcon className={styles.menuIcon} />
+          </button>
+        ) : linksToUse.map(({ text, to }) => (
+          <AnimatedLink className={styles.link} to={to}>{text}</AnimatedLink>
+        ))}
+      </nav>
+
+      {isMobile && (
+        <div className={clsx(styles.sideBar, isSideBarOpen && styles.open)}>
+          <Link to="/">
+            <img className={styles.logo} src={LOGO} alt="HackThis Logo" />
+          </Link>
+
+          <nav>
+            {linksWithHome.map(({ to, text }) => (
+              <HighlightedLink className={styles.link} to={to}>{text}</HighlightedLink>
+            ))}
+          </nav>
+        </div>
+      )}
+
+      {isSideBarOpen && (
+        <button
+          type="button"
+          className={styles.sideBarBackground}
+          onClick={() => setIsSideBarOpen(false)}
+          aria-label="Close Side Bar"
+        />
+      )}
+    </header>
+  );
+};
 
 export default NavBar;
