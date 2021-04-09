91822d1b73a48716cd73344fbdae2ac523a7a3c1
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-04-09 06:07:42 -0500

    Add mentor data + minor style changes

--- src/pages/Mentors/index.tsx
+++ src/pages/Mentors/index.tsx
@@ -8,181 +8,113 @@ import GROUND from '../../assets/mentor/ground.svg';
 import RIGHT_TREE from '../../assets/mentor/right_tree.svg';
 import LEFT_BUSH from '../../assets/mentor/left_bush.svg';
 import RIGHT_BUSH from '../../assets/mentor/right_bush.svg';
-import DROP_ARROW from '../../assets/mentor/drop_arrow.svg';
+// import DROP_ARROW from '../../assets/mentor/drop_arrow.svg';
 
-const categories = [
-  { name: 'Data Science', angle: 10 },
-  { name: 'Languages', angle: -10 },
-  { name: 'Systems', angle: 7 },
-  { name: 'Web Dev', angle: -4 },
-];
+import mentors from './mentorData';
 
-const mentors = [
-  {
-    imageURL: 'https://via.placeholder.com/150',
-    name: 'Mentor 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum purus, quam eu urna suscipit rutrum dolor, sollicitudin amet. Semper eu massa diam habitant ante in turpis elementum. Ullamcorper lorem proin vestibulum massa. Pulvinar at scelerisque id augue ut laoreet. ',
-    category: 0,
-  },
-  {
-    imageURL: 'https://via.placeholder.com/150',
-    name: 'Mentor 2',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum purus, quam eu urna suscipit rutrum dolor, sollicitudin amet. Semper eu massa diam habitant ante in turpis elementum. Ullamcorper lorem proin vestibulum massa. Pulvinar at scelerisque id augue ut laoreet. ',
-    category: 2,
-  },
-  {
-    imageURL: 'https://via.placeholder.com/150',
-    name: 'Mentor 3',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum purus, quam eu urna suscipit rutrum dolor, sollicitudin amet. Semper eu massa diam habitant ante in turpis elementum. Ullamcorper lorem proin vestibulum massa. Pulvinar at scelerisque id augue ut laoreet. ',
-    category: 1,
-  },
-  {
-    imageURL: 'https://via.placeholder.com/150',
-    name: 'Mentor 4',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum purus, quam eu urna suscipit rutrum dolor, sollicitudin amet. Semper eu massa diam habitant ante in turpis elementum. Ullamcorper lorem proin vestibulum massa. Pulvinar at scelerisque id augue ut laoreet. ',
-    category: 2,
-  },
-  {
-    imageURL: 'https://via.placeholder.com/150',
-    name: 'Mentor 5',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum purus, quam eu urna suscipit rutrum dolor, sollicitudin amet. Semper eu massa diam habitant ante in turpis elementum. Ullamcorper lorem proin vestibulum massa. Pulvinar at scelerisque id augue ut laoreet. ',
-    category: 1,
-  },
-  {
-    imageURL: 'https://via.placeholder.com/150',
-    name: 'Mentor 6',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum purus, quam eu urna suscipit rutrum dolor, sollicitudin amet. Semper eu massa diam habitant ante in turpis elementum. Ullamcorper lorem proin vestibulum massa. Pulvinar at scelerisque id augue ut laoreet. ',
-    category: 2,
-  },
-  {
-    imageURL: 'https://via.placeholder.com/150',
-    name: 'Mentor 7',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum purus, quam eu urna suscipit rutrum dolor, sollicitudin amet. Semper eu massa diam habitant ante in turpis elementum. Ullamcorper lorem proin vestibulum massa. Pulvinar at scelerisque id augue ut laoreet. ',
-    category: 1,
-  },
-  {
-    imageURL: 'https://via.placeholder.com/150',
-    name: 'Mentor 8',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum purus, quam eu urna suscipit rutrum dolor, sollicitudin amet. Semper eu massa diam habitant ante in turpis elementum. Ullamcorper lorem proin vestibulum massa. Pulvinar at scelerisque id augue ut laoreet. ',
-    category: 0,
-  },
-];
-const Mentors: React.FC = () => {
-  const [tab, setTab] = useState<number>(0);
-  const [dropped, setDropped] = useState(false);
-  return (
-    <div className={styles.background}>
-      <div
-        className={styles.select_wrapper}
-        style={{ maxHeight: dropped ? '150px' : '45px' }}
+// const [tab, setTab] = useState<number>(0);
+// const [dropped, setDropped] = useState(false);
+const Mentors: React.FC = () => (
+  <div className={styles.background}>
+    {/* <div
+      className={styles.select_wrapper}
+      style={{ maxHeight: dropped ? '150px' : '45px' }}
+    >
+      {categories[tab].name.toUpperCase()}{' '}
+      <span
+        onClick={() => setDropped(!dropped)}
+        tabIndex={0}
+        onKeyPress={() => {}}
+        role="button"
+        className={styles.drop_arrow}
       >
-        {categories[tab].name.toUpperCase()}{' '}
-        <span
-          onClick={() => setDropped(!dropped)}
-          tabIndex={0}
-          onKeyPress={() => {}}
-          role="button"
-          className={styles.drop_arrow}
-        >
-          <img
-            style={{
-              transition: '.5s',
-              height: '100%',
-              transform: `rotate(${dropped ? '180' : '0'}deg)`,
+        <img
+          style={{
+            transition: '.5s',
+            height: '100%',
+            transform: `rotate(${dropped ? '180' : '0'}deg)`,
+          }}
+          src={DROP_ARROW}
+          alt={DROP_ARROW}
+        />
+      </span>
+      <div className={styles.drop_list}>
+        {categories.map((c, i) => (
+          <div
+            key={c.name}
+            onClick={() => {
+              setTab(i);
+              setDropped(false);
             }}
-            src={DROP_ARROW}
-            alt={DROP_ARROW}
-          />
-        </span>
-        <div className={styles.drop_list}>
-          {categories.map((c, i) => (
-            <div
-              key={c.name}
-              onClick={() => {
-                setTab(i);
-                setDropped(false);
-              }}
-              tabIndex={0}
-              onKeyPress={() => {}}
-              role="button"
-              className={styles.drop_category}
-            >
-              {c.name}
-            </div>
-          ))}
-        </div>
+            tabIndex={0}
+            onKeyPress={() => {}}
+            role="button"
+            className={styles.drop_category}
+          >
+            {c.name}
+          </div>
+        ))}
       </div>
-      <div className={styles.shade} />
-      <div className={styles.mobile}>
-        <img src={RIGHT_BUSH} alt="" className={styles.right_bush} />
-        <img src={LEFT_BUSH} alt="" className={styles.left_bush} />
-        <img src={FOREGROUND_TREE} alt="" className={styles.foreground_tree} />
-        <img src={RIGHT_TREE} alt="" className={styles.right_tree} />
-        <img src={LEFT_TREE} alt="" className={styles.left_tree} />
-        <img src={GROUND} alt="" className={styles.ground} />
-        <img src={BUG} alt="" className={styles.bug} />
-        <div className={styles.mentor_type_list}>
-          {categories.map((category, i) => (
-            <div
-              style={{ transform: `rotate(${category.angle}deg)` }}
-              className={[
-                styles.category,
-                i === tab ? styles.category_select : '',
-              ].join(' ')}
-              onKeyPress={() => {}}
-              tabIndex={i}
-              onClick={() => {
-                setTab(i);
-              }}
-              role="button"
-            >
-              {category.name}
-            </div>
-          ))}
-        </div>
-      </div>
+    </div> */}
+    <div className={styles.shade} />
+    <div className={styles.mobile}>
+      <img src={RIGHT_BUSH} alt="" className={styles.right_bush} />
+      <img src={LEFT_BUSH} alt="" className={styles.left_bush} />
+      <img src={FOREGROUND_TREE} alt="" className={styles.foreground_tree} />
+      <img src={RIGHT_TREE} alt="" className={styles.right_tree} />
+      <img src={LEFT_TREE} alt="" className={styles.left_tree} />
+      <img src={GROUND} alt="" className={styles.ground} />
+      <img src={BUG} alt="" className={styles.bug} />
+      {/* <div className={styles.mentor_type_list}>
+        {categories.map((category, i) => (
+          <div
+            style={{ transform: `rotate(${category.angle}deg)` }}
+            className={[
+              styles.category,
+              i === tab ? styles.category_select : '',
+            ].join(' ')}
+            onKeyPress={() => {}}
+            tabIndex={i}
+            onClick={() => {
+              setTab(i);
+            }}
+            role="button"
+          >
+            {category.name}
+          </div>
+        ))}
+      </div> */}
+    </div>
 
-      <div className={styles.container}>
-        <div className={[styles.category_title, styles.mobile].join(' ')}>
-          {categories[tab].name}
-        </div>
-        <div className={styles.shader} />
-        <div className={styles.mentor_list}>
-          {/* Hack to get some extra padding, but also keeps the overflow cutoff at the top of the container */}
-          <br />
-          <br />
-          {mentors.map((mentor) => {
-            if (mentor.category === tab) {
-              return (
-                <div key={mentor.name} className={styles.mentor}>
-                  <img
-                    src={mentor.imageURL}
-                    alt=""
-                    className={styles.mentor_image}
-                  />
-                  <div className={styles.mentor_text}>
-                    <div className={styles.mentor_name}>{mentor.name}</div>
-                    <div className={styles.mentor_description}>
-                      {mentor.description}
-                    </div>
-                  </div>
-                </div>
-              );
-            }
-            return '';
-          })}
-        </div>
+    <div className={styles.container}>
+      <div className={[styles.category_title/* , styles.mobile */].join(' ')}>
+        {/* {categories[tab].name} */}
+        MENTORS
+      </div>
+      <div className={styles.shader} />
+      <div className={styles.mentor_list}>
+        {/* Hack to get some extra padding, but also keeps the overflow cutoff at the top of the container */}
+        <br />
+        <br />
+        {mentors.map((mentor) => (
+          <div key={mentor.name} className={styles.mentor}>
+            <div className={styles.mentor_image} style={{ backgroundImage: `url(${mentor.imageURL})` }} />
+            {/* <img
+              src={mentor.imageURL}
+              alt=""
+              className={styles.mentor_image}
+            /> */}
+            <div className={styles.mentor_text}>
+              <div className={styles.mentor_name}>{mentor.name}</div>
+              <div className={styles.mentor_description}>
+                {mentor.description}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Mentors;
